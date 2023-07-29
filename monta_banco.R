8a0d3eeee05fe2a76c98736bbf60114acfe63292
Bruno Mauricio Rodrigues Crotman <crotman@petrobras.com.br>
2023-07-29 12:11:57 -0300

    ajeitando a bade de preços e receitas

--- monta_banco.R
+++ monta_banco.R
@@ -5,7 +5,6 @@ library(readxl)
 library(janitor)
 
 dados_receita <- read_excel("dados/revisao_receita_bbg.xlsx") %>% 
-    select(-c(1,2)) %>% 
     slice(-1) %>% 
     rename(datas = 1) %>% 
     mutate(
@@ -21,8 +20,12 @@ dados_receita <- read_excel("dados/revisao_receita_bbg.xlsx") %>%
     ) %>% 
     filter(
         !is.na(receita)
+    ) %>% 
+    mutate(
+        receita = as.numeric(receita)
     )
-
+    
+    
 
 dados_juros = read_excel(
     "dados/Dados_4Y_DI_NTNB.xlsx"
@@ -32,8 +35,9 @@ dados_juros = read_excel(
 
 
 dados_preco <- read_csv2("dados/dados_2015_2018.csv") %>% 
-    bind_rows("dados/dados_2002_2005_new.csv" %>%  read_csv2()) %>% 
+    bind_rows("dados/dados_2002_2005.csv" %>%  read_csv2()) %>% 
     bind_rows("dados/dados_2009_2011.csv" %>%  read_csv2()) 
+    bind_rows("dados/dados_2011_2012.csv" %>%  read_csv2()) 
 
 
 write_rds(dados_receita, "dados-preparados/dados_receita.rds" )
